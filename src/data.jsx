be68d76e64aa85cdb4356d72f49b67b5e41e54a1
“Elena” <ellenka871@gmail.com>
2025-09-19 16:54:20 -0700

    Image update

--- src/data.jsx
+++ src/data.jsx
@@ -4,70 +4,70 @@ export const data = [
     name: "Nano PC",
     searchTerm: "nano",
     price: 999.99,
-    image: "http://image.shuttle.com/ResourceCenter/Upload/2853/2853_WebImage_WebImage_202411081739_1"
+    image: "/images/2853_WebImage.jpg"
 },
 {
     id: 2,
     name: "AI PC",
     searchTerm: "slim",
     price: 999.99,
-    image: "http://image.shuttle.com/ResourceCenter/Upload/2905/2905_WebImage_WebImage_202411061643_2"
+    image: "/images/2905_WebImage.jpg"
 },
 {
     id: 3,
     name: "Cube PC",
     searchTerm: "cube",
     price: 999.99,
-    image: "http://image.shuttle.com/ResourceCenter/Upload/2623/2623_WebImage_WebImage_202108251513_1"
+    image: "/images/2623_WebImage.jpg"
 },
 {
     id: 4,
     name: "Slim PC",
     searchTerm: "slim",
     price: 999.99,
-    image: "http://image.shuttle.com/ResourceCenter/Upload/2909/2909_WebImage_WebImage_202411191301_2"
+    image: "/images/2909_WebImage.jpg"
 },
 {
     id: 5,
     name: "Fanless PC",
     searchTerm: "slim",
     price: 999.99,
-    image: "http://image.shuttle.com/ResourceCenter/Upload/2764/2764_WebImage_WebImage_202304191347_1"
+    image: "/images/2764_WebImage.jpg"
 },
 {
     id: 6,
     name: "AIO PC",
     searchTerm: "aio",
     price: 999.99,
-    image: "http://image.shuttle.com/ResourceCenter/Upload/2847/2847_WebImage_WebImage_202405221836_1"
+    image: "/images/2847_WebImage.jpg"
 },
 {
     id: 7,
     name: "Embedded Box PC",
     searchTerm: "ipc",
     price: 999.99,
-    image: "http://image.shuttle.com/ResourceCenter/Upload/2779/2779_WebImage_WebImage_202311070954_2"
+    image: "/images/2779_WebImage.jpg"
 },
 {
     id: 8,
     name: "Edge IoT PC",
     searchTerm: "ipc",
     price: 999.99,
-    image: "http://image.shuttle.com/ResourceCenter/Upload/2861/2861_WebImage_WebImage_202407020919_5"
+    image: "/images/2861_WebImage.jpg"
 },
 {
     id: 9,
     name: "Touch Panel PC",
     searchTerm: "aio",
     price: 999.99,
-    image: "http://image.shuttle.com/ResourceCenter/Upload/2751/2751_WebImage_WebImage_202311221036_1"
+    image: "/images/2751_WebImage.jpg"
 },
 {
     id: 10,
     name: "Nano XPC",
     searchTerm: "nano",
     price: 999.99,
-    image: "http://image.shuttle.com/ResourceCenter/Upload/2731/2731_WebImage_WebImage_202208291447_1"
+    image: "/images/2731_WebImage.jpg"
 },
 // {
 //     id: 1,
